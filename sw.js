6ac95ada0d2e58e155d3df5878b38fb47ce153aa
fuler1234-creator <fuler1234@gmail.com>
2026-05-31 08:39:43 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+var CACHE = 'dochazka-v1';
+var FILES = ['./dochazka.html', './manifest.json'];
+
+self.addEventListener('install', function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(FILES); }));
+});
+
+self.addEventListener('fetch', function(e){
+  e.respondWith(caches.match(e.request).then(function(r){ return r || fetch(e.request); }));
+});
